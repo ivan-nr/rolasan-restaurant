d856781eded67036c70736dbe0de88b1f7763b60
ivannr22 <ivanngudi16@gmail.com>
2023-11-30 14:33:04 +0700

    add menu section

--- js/script.js
+++ js/script.js
@@ -5,11 +5,19 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// klik diluar sidebar menu, menghilangkan nav
+// klik diluar sidebar menu, menyembunyikan nav
 const hamburger = document.querySelector("#hamburger-menu");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+});
+
+// perubahan background navbar ketika di scroll
+document.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  window.scrollY > 0
+    ? navbar.classList.add("scrolled")
+    : navbar.classList.remove("scrolled");
 });
